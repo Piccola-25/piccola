e47ef7d4b8cd0bc0b9878b1209cabf3d1b51f17c
Aman Kumar <1211aman.k@gmail.com>
2025-10-22 00:45:50 +0530

    aman-dev added support for sanity & navbar

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import category from "./category";
+import homePageImage from "./homePageImage";
+import products from "./products";
+
+export const schemaTypes = [products, category, homePageImage]
