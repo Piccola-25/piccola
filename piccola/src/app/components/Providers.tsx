e5236494d9990876287706da06197b2bc1b33544
Prajjwal Maurya <1112prajwal.m@gmail.com>
2025-10-25 23:46:37 +0530

    aman-dev: Added cart support

--- piccola/src/app/components/Providers.tsx
+++ piccola/src/app/components/Providers.tsx
@@ -1,1 +1,18 @@
-export default
+'use client';
+
+import { CartProvider as StoreCartProvider } from 'use-shopping-cart';
+
+
+export default function CartProvider({children}:{children: React.ReactNode}) {
+    return <StoreCartProvider mode='payment' 
+    cartMode='client-only' 
+    stripe='{process.env.NEXT_PUBLIC_STRIPE_KEY}' currency='USD'
+    successUrl={'http://localhost:3000/success'}
+    cancelUrl={'http://localhost:3000/canceled'}
+    billingAddressCollection={true}
+    shouldPersist={true}
+    language='en-US'
+    >
+        {children}
+        </StoreCartProvider>
+}
